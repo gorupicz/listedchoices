bedc2a141e6133a728bf25cd0f75ec9d7e5745f5
Esteban <esteban@atexto.com>
2024-12-11 15:00:42 -0500

    added pok-ta-pok listing

--- mongodb/playground-1.mongodb.js
+++ mongodb/playground-1.mongodb.js
@@ -13,37 +13,17 @@
 use('bolsadecasas');
 
 // Update the document with the specified _id
-db.getCollection('listings').updateOne(
-    { _id: ObjectId("673f6568cf409cc71082b57a") },
-    {
-        $unset: { "photos": "" }
-    }
+
+// Find the document with the specified _id
+const originalDocument = db.getCollection('listings').findOne(
+    { _id: ObjectId("673f6568cf409cc71082b57a") }
 );
 
-db.getCollection('listings').updateOne(
-    { _id: ObjectId("673f6568cf409cc71082b57a") },
-    {
-        $set: {
-            "photos": [
-                { "img": "https://a0.muscache.com/im/pictures/miso/Hosting-47826981/original/0a233e66-77b4-42a8-b65d-163c91123f83.jpeg" },
-                { "img": "https://a0.muscache.com/im/pictures/miso/Hosting-47826981/original/e03d6b36-80e7-4574-9455-c2516f248eb0.jpeg" },
-                { "img": "https://a0.muscache.com/im/pictures/miso/Hosting-47826981/original/76eb6baf-fca9-4ff8-8d67-88b0ddb9f5cc.jpeg" },
-                { "img": "https://a0.muscache.com/im/pictures/miso/Hosting-47826981/original/1ca24523-9427-409d-8664-259dd505e37c.jpeg" },
-                { "img": "https://a0.muscache.com/im/pictures/miso/Hosting-47826981/original/02838c8f-2817-4b6c-b12d-dcfbb66675f5.jpeg" },
-                { "img": "https://a0.muscache.com/im/pictures/miso/Hosting-47826981/original/9be7f08d-eb3c-4364-91e4-12c65928b857.jpeg" },
-                { "img": "https://a0.muscache.com/im/pictures/miso/Hosting-47826981/original/db55f7bf-13b1-40d7-b250-13008fb10120.jpeg" },
-                { "img": "https://a0.muscache.com/im/pictures/miso/Hosting-47826981/original/ff695b9a-3dbb-43ad-ab52-07d65349d9d5.jpeg" },
-                { "img": "https://a0.muscache.com/im/pictures/miso/Hosting-47826981/original/2daeb073-0595-4c03-8243-2975c601e808.jpeg" },
-                { "img": "https://a0.muscache.com/im/pictures/miso/Hosting-47826981/original/9dc92703-79e7-4447-8997-ac4372d5a026.jpeg" },
-                { "img": "https://a0.muscache.com/im/pictures/miso/Hosting-47826981/original/e95076c2-bd11-45fc-82e1-a018b179217a.jpeg" },
-                { "img": "https://a0.muscache.com/im/pictures/miso/Hosting-47826981/original/82393d4d-74e0-4ec9-9aed-5bedec8e61a6.jpeg" },
-                { "img": "https://a0.muscache.com/im/pictures/miso/Hosting-47826981/original/d1e86d51-4d33-4ec7-866c-390aa7322e88.jpeg" },
-                { "img": "https://a0.muscache.com/im/pictures/miso/Hosting-47769930/original/daa0f374-5820-4d75-a5ce-b9b7384e234a.jpeg" },
-                { "img": "https://a0.muscache.com/im/pictures/miso/Hosting-47769930/original/ab98befe-2bc6-4614-a303-9c868d3055a5.png" }
-            ]
-        }
-    }
-);
+// Remove the _id field from the original document to avoid duplicate key error
+delete originalDocument._id;
+
+// Insert the document as a new entry
+db.getCollection('listings').insertOne(originalDocument);
 
 const allListings = db.getCollection('listings').find({}).toArray();
 
